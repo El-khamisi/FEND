e1b50c2f81ea5d9d698b62c95c12a077625eba43
El-khamisi <mohammd.alkhamisi@gmail.com>
2021-05-11 18:36:59 +0200

    Initialize skeleton

---
+++ weather-journal-app/website/app.js
@@ -0,0 +1,21 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = 'da37898d48db505fe667d173abc72170';
+const baseURL = 'api.openweathermap.org/data/2.5/weather';
+const APIcall = `api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`;
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
