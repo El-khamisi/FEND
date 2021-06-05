b6cf6bf84984459e73ffbe8ea885d566ec0f3643
El-khamisi <mohammd.alkhamisi@gmail.com>
2021-06-05 04:30:53 +0200

    init

---
+++ evaluate-news-nlp/src/server/index.js
@@ -0,0 +1,29 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+const application_key = process.env.API_KEY;
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile('dist/index.html')
+        // res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function() {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function(req, res) {
+    res.send(mockAPIResponse)
+})
